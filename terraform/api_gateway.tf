a22766b5e2b131b3a650e3a6e323d325aabe2b6a
Sarneet Singh <sarneets@Sarneets-MacBook-Air.local>
2026-02-21 17:12:15 +0530

    fix: CORS support + auto frontend deploy on push

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -31,7 +31,12 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 
 resource "aws_api_gateway_deployment" "contact_deployment" {
     rest_api_id = aws_api_gateway_rest_api.contact_api.id
-    depends_on = [ aws_api_gateway_method.contact_post, aws_api_gateway_integration.lambda_integration ]
+    depends_on = [
+    aws_api_gateway_method.contact_post,
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_method.options,
+    aws_api_gateway_integration_response.options,
+  ]
 }
 
 resource "aws_api_gateway_stage" "contact_stage" {
@@ -47,4 +52,50 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = aws_lambda_function.contact_handler.function_name
   principal = "apigateway.amazonaws.com"
   source_arn = "${aws_api_gateway_rest_api.contact_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = aws_api_gateway_rest_api.contact_api.id
+  resource_id   = aws_api_gateway_resource.contact_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options" {
+  rest_api_id = aws_api_gateway_rest_api.contact_api.id
+  resource_id = aws_api_gateway_resource.contact_resource.id
+  http_method = aws_api_gateway_method.options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.contact_api.id
+  resource_id = aws_api_gateway_resource.contact_resource.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.contact_api.id
+  resource_id = aws_api_gateway_resource.contact_resource.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_integration.options]
 }
